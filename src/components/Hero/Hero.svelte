45229b0027dacb961b9bc93e37ef38d7e1714765
joerull-st <joe.rull@startribune.com>
2025-10-21 15:50:27 -0500

    making stuff

--- src/components/Hero/Hero.svelte
+++ src/components/Hero/Hero.svelte
@@ -39,48 +39,44 @@ Renders a visual replica of the Immersive Template hero.
   } = $props();
 </script>
 
-<Grid additionalClasses="gap-y-8 md:gap-y-10">
-  <GridRow>
-    <div class="flex justify-center my-6">
-      <SectionLabel>none</SectionLabel>
-    </div>
-    <div class="flex flex-col gap-y-6 md:gap-y-8 text-center">
-      <Headline>{headline}</Headline>
-      <Timestamp>{timestamp}</Timestamp>
-    </div>
-  </GridRow>
 
-  {#if heroImageUrl}
-    <GridRow variant={"fullBleed"}>
-      <Image
-        variant={"captionCentered"}
-        src={heroImageUrl}
-        alt={heroImageAltText}
-        caption={heroImageCaption}
-      />
-    </GridRow>
-  {/if}
 
-  <GridRow
-    additionalClasses="justify-self-center md:max-w-[535px] md:text-center lg:max-w-[712px]"
-  >
-    <div class="flex flex-col gap-y-5 md:justify-items-center md:items-center">
-      <Dek>
-        {dek}
-      </Dek>
-      <div class="flex items-center w-full gap-x-2 md:justify-center">
-        <div class="flex flex-col gap-y-0.5 gap-x-1 md:flex-row">
-          {#if authorName}
-            <div class="flex">
-              <Byline href={authorBioUrl}>By {authorName}</Byline>
-            </div>
-          {/if}
+<style>
+div.byline {
+  font-family: "Graphik";
+}
 
-          <span class="text-text-secondary font-utility-label-reg-02">
-            The Minnesota Star Tribune
-          </span>
-        </div>
-      </div>
-    </div>
-  </GridRow>
-</Grid>
+div.dateline {
+ letter-spacing: 3px;
+}
+
+div.subhead {
+}
+
+</style>
+
+
+  <GridRow variant={"fullBleed"}>
+<div class="md:flex xs:block mb-4">
+  <div class="w-1/2 xs:w-full bg-black md:h-[95vh] md:min-h-[825px] xs:h-[525px] text-white">
+
+  <h1 class="md:pt-[140px] xs:pt-[50px] md:text-[3.5rem] xs:text-[2.75rem] max-w-[440px] px-4 font-[publico-banner-light-italic] mx-auto text-center">The Red Lake Nation football team hasn't won a game in years, but its coach keeps his kids undefeated</h1>
+      <div class="dateline mx-auto text-center uppercase !text-[12px] pt-3">December 2, 2022</div>
+
+
+
+  <div class="byline mx-auto text-center pt-16 !text-[15px]">By <b>Chip Scoggins</b> Star Tribune</div>
+  <div class="byline mx-auto text-center pt-1 !text-[15px]"> Photos by <b>Aaron Lavinksy</b> Star Tribune</div>
+
+  
+
+
+  
+  
+  </div>
+  <div class="w-1/2 xs:w-full  bg-[url(https://ststatic.stimg.co/images/migrated-directus-images/redlake_sidehero.jpg)] bg-cover bg-right md:h-[95vh] md:min-h-[825px] xs:h-[140vw]"></div>
+</div>
+
+  <div class="subhead mx-auto font-[publico-text-roman] text-[1.75rem] max-w-[850px] text-center pt-12 pb-8 leading-[1.5]">They often play against opponents with twice as many players. Sometimes, coach Nolan Desjarlait wonders how he keeps doing it. The answer always comes: &ldquo;I refuse to give up on them.&rdquo;</div> 
+
+</GridRow>
